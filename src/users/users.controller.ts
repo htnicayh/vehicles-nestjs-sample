f1dbed2296fb1ee7cc64c1c9dc5244ec39142a89
htnicayh <hieuhpcf@gmail.com>
2021-10-02 00:40:45 +0700

    build: add hooks

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,11 +1,16 @@
 import { Controller, Post, Body } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
+import { UsersService } from './users.service';
 
 @Controller('auth')
 export class UsersController {
+    constructor(private readonly usersService: UsersService) {}
+
     @Post('/sign-up')
     createUser(@Body() body: CreateUserDto) {
-        console.log(body)
+        const { email, password } = body
+
+        this.usersService.create(email, password)
     } 
 
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,19 @@
 import { Injectable } from '@nestjs/common';
+import { Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
+import { UserEntity } from './entity/users.entity';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+    constructor(
+        @InjectRepository(UserEntity) private readonly Repo: Repository<UserEntity>
+    ) {
+
+    }
+
+    create(email: string, password: string) {
+        const newUser = this.Repo.create({ email, password })
+
+        return this.Repo.save(newUser)
+    }
+}
