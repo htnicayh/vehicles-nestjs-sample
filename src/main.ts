34d31048a88640c0e9f228916e8eea15f5ee1b95
htnicayh <hieuhpcf@gmail.com>
2021-10-04 18:19:04 +0700

    install: @types/session-cookie

--- src/main.ts
+++ src/main.ts
@@ -2,19 +2,19 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
 import { ValidationPipe } from '@nestjs/common'
 
+const CookieSession = require('cookie-session')
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(CookieSession({
+    keys: ['znoktob']
+  }))
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true // avoid orther infomation in your req.body
     })
   )
-  await app.listen(3001);
+  await app.listen(3000);
 }
 bootstrap();
 
-// Users : Sign-In, Sign-Up -> Controller, Service, Entity (To Database), Repository
-// Reports: Create, Update, Delete, Find -> Controller, Service, Entity, Repository
-// ->> UsersModule - ReportsModule
-// Entity (Database) -> createConnection (forRoot in AppModule)
-// TypeOrmModule forFeature -> Both UserEntity & Reports Entity (of UserModule & ReportModule)
