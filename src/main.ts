18ec2c74a4333bb4f760d4d99dcb5df71ae22113
htnicayh <hieuhpcf@gmail.com>
2021-10-09 17:39:36 +0700

    rafactor: remove require cookie-session

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
 
-const CookieSession = require('cookie-session')
-
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
